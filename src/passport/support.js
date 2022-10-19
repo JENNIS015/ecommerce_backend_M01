f70bed8c314161d8e819e4a8b1d910f50fc99ae8
Jennifer <jennis015@hotmail.com>
2022-10-19 08:18:56 -0300

    faltaba importar logger

--- src/passport/support.js
+++ src/passport/support.js
@@ -1,13 +1,16 @@
-function isAdmin(req, res, next) {
-  logger.info('Identificado', req.isAuthenticated());
-  logger.info('Membresia', req.user.userDetail.membershipID);
-  if (req.isAuthenticated() && req.user.userDetail.membershipID === 1) {
-    return next();
-  } else {
-    res.status(403).json({
-      error: 'se requiere autenticacion  de admin para acceder a este recurso',
-    });
-  }
-}
+const logger = require('../../src/utils/loggers')
+
+  function isAdmin(req, res, next) {
+    logger.info('Identificado', req.isAuthenticated());
+    logger.info('Membresia', req.user.userDetail.membershipID);
+    if (req.isAuthenticated() && req.user.userDetail.membershipID === 1) {
+      return next();
+    } else {
+      res.status(403).json({
+        error:
+          'se requiere autenticacion  de admin para acceder a este recurso',
+      });
+    }
+  };
 
 module.exports = { isAdmin };
