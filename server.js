7b654d1be65d3568c93402f029bc8a0d660a2dbe
Jennifer <jennis015@hotmail.com>
2022-10-19 08:26:32 -0300

    cambios en cookie

--- server.js
+++ server.js
@@ -86,9 +86,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      maxAge: 1000 * 60 * 60 * 24 * 7, // one day
-      secure: false,
-      httpOnly: false,
+      secure: true
     },
   })
 );
