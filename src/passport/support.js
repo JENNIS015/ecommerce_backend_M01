06d1aaadf28af2dd05a41f2eee8aeb3f81bec15f
Jennifer <jennis015@hotmail.com>
2022-10-19 23:36:00 -0300

    support

--- src/passport/support.js
+++ src/passport/support.js
@@ -1,10 +1,10 @@
 const logger = require('../../src/utils/loggers')
 
   function isAdmin(req, res, next) {
-    logger.info('Identificado', req.isAuthenticated());
-    logger.info('Membresia', req);
-        logger.info('Membresia', req.passport);
-    if (req.isAuthenticated() && req._passport.session.user.membershipID === 1) {
+    logger.info('Identificado', req.isAuthenticated);
+    logger.info('Membresia', req._passport);
+        logger.info('Membresia', req);
+    if (req.isAuthenticated() && req.user.membershipID === 1) {
       return next();
     } else {
       res.status(403).json({
